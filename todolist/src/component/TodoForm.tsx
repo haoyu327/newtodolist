b78c8faf80aa212ee91d1a4c12c82110d99cf3e6
haoyu327 <75198635+haoyu327@users.noreply.github.com>
2021-06-06 19:25:54 +0800

    todolist

---
+++ todolist/src/component/TodoForm.tsx
@@ -0,0 +1,72 @@
+import React, { Component } from 'react'
+import Todolist from './Todolist'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
+
+const styles = {
+  todoForm: {
+    margin: '0 0 30px 0 ',
+  },
+  textInput: {
+    width: '50%',
+  },
+  submit: {
+    padding: '16px 16px',
+    backgroundColor: '#274472',
+    color: 'white',
+  },
+}
+export interface TodoModel {
+  id: number
+  title: string
+}
+class TodoForm extends Component {
+  state: TodoModel[] = [
+    {
+      id: 1,
+      title: '123',
+    },
+    {
+      id: 2,
+      title: '123',
+    },
+  ]
+  value = ''
+
+  handelInput(title: string) {
+    this.value = title
+  }
+  submit() {
+    this.state.push({
+      title: this.value,
+      id: this.state.length + 1,
+    })
+
+    console.log(this.state)
+  }
+  render() {
+    return (
+      <div>
+        <form style={styles.todoForm}>
+          <TextField
+            id='outlined-basic'
+            label='Outlined'
+            variant='outlined'
+            onChange={(e) => this.handelInput(e.target.value)}
+            style={styles.textInput}
+          />
+          <Button
+            onClick={() => this.submit()}
+            style={styles.submit}
+            variant='contained'
+          >
+            Submit
+          </Button>
+        </form>
+        <Todolist todos={this.state}></Todolist>
+      </div>
+    )
+  }
+}
+
+export default TodoForm
